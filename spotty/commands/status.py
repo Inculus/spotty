56863012668a6c13ad13c2a04f900047e229fbe6
Tarang <Tarang@users.noreply.github.com>
2020-02-08 21:54:08 +1100

    Update status.py

--- spotty/commands/status.py
+++ spotty/commands/status.py
@@ -1,4 +1,4 @@
-from argparse import Namespace
+from argparse import Namespace, ArgumentParser
 from spotty.commands.abstract_config_command import AbstractConfigCommand
 from spotty.commands.writers.abstract_output_writrer import AbstractOutputWriter
 from spotty.providers.abstract_instance_manager import AbstractInstanceManager
@@ -9,5 +9,12 @@ class StatusCommand(AbstractConfigCommand):
     name = 'status'
     description = 'Print information about the instance'
 
+    def configure(self, parser: ArgumentParser):
+        super().configure(parser)
+        parser.add_argument('--ip', action='store_true', help='Show public ip address')
+
     def _run(self, instance_manager: AbstractInstanceManager, args: Namespace, output: AbstractOutputWriter):
-        output.write(instance_manager.get_status_text())
+        if args.ip == True:
+            output.write(instance_manager.get_public_ip_address())
+        else:
+            output.write(instance_manager.get_status_text())
